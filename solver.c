f8992175962acfbcbad204403179fa9fc30c5ee9
Arush R Katrekar <akatreka@eceprog5.ecn.purdue.edu>
2026-04-01 13:05:36 -0400

    fix: containsNode infinite loop

--- solver.c
+++ solver.c
@@ -22,8 +22,38 @@ PathLL* solveMaze(Maze* m) {
 
 void depthFirstSolve(Maze* m, MazePos curpos, char* path, int step,
                      PathLL* successPaths) {
+    if (atEnd(curpos,m)) {
+        path[step] = '\0';
+        addNode(successPaths, path);
+        return;
+    }
+    m->maze[curpos.ypos][curpos.xpos].visited = true;
 
-    // TODO (Your best bet is to modify a working implementation from HW9)
+    //North
+    MazePos next = {curpos.xpos,curpos.ypos - 1};
+    if (squareOK(next,m)) {
+        path[step] = 'N';
+        depthFirstSolve(m, next, path, step + 1, successPaths);
+    }
+    //South
+    next = (MazePos){curpos.xpos, curpos.ypos + 1};
+    if (squareOK(next,m)) {
+        path[step] = 'S';
+        depthFirstSolve(m, next, path, step + 1, successPaths);
+    }
+    //East
+    next = (MazePos){curpos.xpos + 1, curpos.ypos};
+    if (squareOK(next,m)) {
+        path[step] = 'E';
+        depthFirstSolve(m, next, path, step + 1, successPaths);
+    }
+    //West
+    next = (MazePos){curpos.xpos - 1, curpos.ypos};
+    if (squareOK(next,m)) {
+        path[step] = 'W';
+        depthFirstSolve(m, next, path, step + 1, successPaths);
+    }
+    m->maze[curpos.ypos][curpos.xpos].visited = false;
 
     return;
 }
